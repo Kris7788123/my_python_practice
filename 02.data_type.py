02d8b56222240f46946b2679ba3d43ab24273526
Kris7788123 <130820200+Kris7788123@users.noreply.github.com>
2023-04-15 14:44:30 +0800

    learn data type
    1.string 2.int 3.type() 4.list 5.set 6.dictionary

---
+++ 02.data_type.py
@@ -0,0 +1,36 @@
+a = "123"  #這是123是字串
+b = 123    #數字格式
+
+print(f'a:{a}\n')
+print(f'b:{b}\n')
+
+type_a = type(a)
+type_b = type(b)
+
+print(f'a:{a} ,type{type_a}\n')
+print(f'b:{b} ,type{type_b}\n')
+
+#有順序、可動的列表 List
+list_a = [3,4,5, 6, 7, 8]
+list_b = ["hello", "normal", "dsasda", "banana"]
+
+print(list_a)
+print(list_b)
+
+print(type(list_b)) 
+
+#集合set 
+set_a = {345}
+set_b = {"hello", "normal"}
+
+print(f'set a : {set_a}')
+print(f'set b : {set_b}')
+print("--------------------------")
+#字典 dictionary
+dictionary_a ={
+    "key":"value", 
+    "apple":"蘋果", 
+    "data":"資料"
+}
+for i in dictionary_a:
+    print(f'key: {i}, value: {dictionary_a[i]}\n')
